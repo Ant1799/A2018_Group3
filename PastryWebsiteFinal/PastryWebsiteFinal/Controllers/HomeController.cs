28c7cabb07bba73b6cda4369a00636cb3a03eb49
UnknownOtagomez <95616744+Howell09@users.noreply.github.com>
2022-02-18 05:39:18 +0800

    Pastry Website

---
+++ PastryWebsiteFinal/PastryWebsiteFinal/Controllers/HomeController.cs
@@ -0,0 +1,345 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PastryWebsiteFinal.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult TutorialsSection()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+
+        public ActionResult TutorialDetails()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult CakesSection()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult CookiesSection()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult CupcakesSection()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult BreadsSection()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult PinoyDelicaciesSection()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult CakesTutorial1()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+
+        public ActionResult CakesTutorial2()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult CakesTutorial3()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult CakesTutorial4()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult CakesTutorial5()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult CakesTutorial6()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult CakesTutorial7()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult CakesTutorial8()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult CakesTutorial9()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult CookiesTutorial1()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult CookiesTutorial2()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult CookiesTutorial3()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult CookiesTutorial4()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult CookiesTutorial5()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult CookiesTutorial6()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult CookiesTutorial7()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult CookiesTutorial8()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult CookiesTutorial9()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult CupcakesTutorial1()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult CupcakesTutorial2()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult CupcakesTutorial3()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult CupcakesTutorial4()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult CupcakesTutorial5()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult CupcakesTutorial6()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult CupcakesTutorial7()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult CupcakesTutorial8()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult CupcakesTutorial9()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult BreadsTutorial1()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult BreadsTutorial2()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult BreadsTutorial3()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult BreadsTutorial4()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult BreadsTutorial5()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult BreadsTutorial6()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult BreadsTutorial7()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult BreadsTutorial8()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult BreadsTutorial9()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult PinoyDelicaciesTutorial1()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult PinoyDelicaciesTutorial2()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult PinoyDelicaciesTutorial3()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult PinoyDelicaciesTutorial4()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult PinoyDelicaciesTutorial5()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult PinoyDelicaciesTutorial6()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult PinoyDelicaciesTutorial7()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult PinoyDelicaciesTutorial8()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+        public ActionResult PinoyDelicaciesTutorial9()
+        {
+            ViewBag.Message = "Your tutorials page.";
+
+            return View();
+        }
+    }
+}
